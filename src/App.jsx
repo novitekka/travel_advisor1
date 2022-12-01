c6a2d5b087bd26364eee56bf272b5226d2533d73
Novit <novitekka@gmail.com>
2022-12-01 22:02:30 +0530

    Initializing Project

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { CssBaseline, Grid } from '@material-ui/core';
+import Header from './components/Header/Header.jsx';
+import List from './components/List/List.jsx';
+import Map from './components/Map/Map.jsx';
+
+
+
+const App=() => {
+    return (
+        <>
+       <CssBaseline />
+       <Header />
+       <Grid container spacing = {3} style={{width:'100%'}}>
+        <Grid item xs={12} md={4}>
+            <List />
+        </Grid>
+        <Grid item xs={12} md={4}>
+            <Map />
+        </Grid>
+        
+        </Grid> 
+      
+    </>
+    );
+}
+
+export default App;
