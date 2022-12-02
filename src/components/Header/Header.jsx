73775a9bb6e7304f0990be791955f20c41fd8eda
vbhv4GitHub <ravi9406126068@gmail.com>
2022-12-02 23:07:29 +0530

    fix(deps): :construction_worker: fixed the package.json file

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,35 +1,30 @@
 import React from 'react';
-// import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
-
 import useStyles from './styles.js';
 
 const Header = () => {
   const classes = useStyles();
 
   return (
-    <h1>Hello</h1>
-    // <AppBar position="static">
-    //   <Toolbar className={classes.toolbar}>
-    //     <Typography variant="h5" className={classes.title}>
-    //       Travel Advisor
-    //     </Typography>
-    //     <Box display="flex">
-    //       <Typography variant="h6" className={classes.title}>
-    //         Explore new places
-    //       </Typography>
-    //       {/* <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}> */}
-    //         <div className={classes.search}>
-    //           <div className={classes.searchIcon}>
-    //             <SearchIcon />
-    //           </div>
-    //           <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
-    //         </div>
-    //       {/* </Autocomplete> */}
-    //     </Box>
-    //   </Toolbar>
-    // </AppBar>
+    <AppBar position='static'>
+      <Toolbar className={classes.toolbar}>
+        <Typography variant='h5' className={classes.title}>
+          Travel Advisor
+        </Typography>
+        <Box display='flex'>
+          <Typography variant='h6' className={classes.title}>
+            Explore new places
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase placeholder='Search…' classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+          </div>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
