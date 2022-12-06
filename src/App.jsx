d4008ac6c685aeb3a3e6c137895839d0ae348ad4
Novit <novitekka@gmail.com>
2022-12-06 16:53:23 +0530

    Google map api working, working on list started

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { CssBaseline, Grid } from '@material-ui/core';
+import { CssBaseline, Grid} from '@material-ui/core';
 import Header from './components/Header/Header.jsx';
 import List from './components/List/List.jsx';
 import Map from './components/Map/Map.jsx';
@@ -15,7 +15,7 @@ const App=() => {
         <Grid item xs={12} md={4}>
             <List />
         </Grid>
-        <Grid item xs={12} md={4}>
+        <Grid item xs={12} md={8}>
             <Map />
         </Grid>
         
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,29 @@
 import React from "react";
-const Map = () =>{
+import GoogleMapReact from "google-map-react";  
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab";
+
+import useStyles from './styles';
+const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+    const coordinates={lat:0, lng:0};
     return (
-        <h1>Map</h1>
+        <div className={classes.mapContainer}>
+        <GoogleMapReact
+            bootstrapURLKeys={{key:'AIzaSyCfg-sdktQP8irXdOHd_Q_GEu7ecQ8wCJA'}}
+            defaultCenter={coordinates}
+            center={coordinates}
+            defaultZoom={14}
+            margin={[50,50,50,50]}
+            // options={''}
+            // onChange={''}
+            // onChildClick={''}
+        >
+        </GoogleMapReact>
+        
+        </div>
     );
     
 }
